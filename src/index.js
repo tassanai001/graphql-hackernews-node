12be2fdcbf3e4b63d59a8d8f36cac943c0477755
tassanai <tassanai.yeeton@gmail.com>
2018-05-14 23:11:41 +0700

    Add: Query && Mutation

--- src/index.js
+++ src/index.js
@@ -1,19 +1,58 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}];
 
+let idCount = links.length;
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Mutation: {
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link);
+      return link;
+    },
+    deleteLink: (root, args) => {
+      links.filter((o) => o.id !== args.id);
+      let newValue = [];
+      links.forEach((o) => {
+        if (o.id !== args.id) {
+        } else {
+          newValue.push(o);
+        }
+      });
+      return newValue[0];
+    },
+    updateLink: (root, args) => {
+      links.filter((o) => o.id !== args.id);
+      let newValue = [];
+      links.forEach((o, i) => {
+        if (o.id === args.id) {
+          links[i] = {
+            id: args.id,
+            description: args.description,
+            url: args.url,
+          }
+          newValue.push(links[i]);
+        }
+      });
+      return newValue[0];
+    }
   }
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 
